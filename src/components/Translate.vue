54d6e5d1f3f00dd96fa72e28f3dea6330218cb5f
Joowon Lee <marulover7@gmail.com>
2019-07-10 14:34:43 +0900

    S1P1112036-19 자동번역 구현 script를 Translate.vue가 아닌 index.html에 붙여쥐

---
+++ src/components/Translate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button id="google_translate_element" v-on:click="googleTranslateElementInit">Translate!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Translate',
+  methods: {
+    googleTranslateElementInit() {
+      new google.translate.TranslateElement({
+      }, "google_translate_element");
+    }
+  }
+};
+</script>
+
+
