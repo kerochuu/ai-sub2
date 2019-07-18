334d46f2a0ae655ebd77c7caeca4217a8d53be1c
Joowon Lee <marulover7@gmail.com>
2019-07-18 14:18:46 +0900

    portfolio write page 수정 사진첨부 아직 미완성

--- src/views/PortfolioWriterPage.vue
+++ src/views/PortfolioWriterPage.vue
@@ -1,40 +1,57 @@
 <template>
-<v-layout mt-5 wrap>
-  <v-flex xs12>
-    <ImgBanner imgSrc="https://source.unsplash.com/5brvJbR1Pn8/1600x900">
-      <div style="line-height:1.2em;font-size:1.2em;" slot="text">Write your Portfolio</div>
-    </ImgBanner>
-  </v-flex>
+  <div>
+    <div id="portfolioHeader">
+      <h1 style="font-size:8vh; text-align: center;">Write Your Portfolio</h1>
+    </div>
+    <div id="portfolioWrite">
+      <!-- <v-layout wrap id="writeCard"> -->
+      <div id="writeCard">
+        <v-flex>
+          <v-text-field
+            label="Portfolio title"
+            placeholder="Portfolio title"
+            outline
+            v-model="title"
+          ></v-text-field>
+        </v-flex>
 
-  <v-flex xs12>
-    <v-text-field label="Portfolio title" placeholder="Portfolio title" outline v-model="title"></v-text-field>
-  </v-flex>
+        <v-flex>
+          <v-textarea
+            auto-grow="true"
+            outline
+            name="Portfolio Description"
+            label="Portfolio Description"
+            v-model="body"
+            @input="update"
+          ></v-textarea>
+        </v-flex>
 
-  <v-flex xs6>
-    <v-textarea auto-grow="true" outline name="Portfolio Description" label="Portfolio Description" v-model="body" @input="update"></v-textarea>
-  </v-flex>
-  <v-flex xs6>
-    <div v-html="compiledMarkdown"></div>
-  </v-flex>
+        <v-layout wrap>
+          <v-flex xs6>
+            <div v-html="compiledMarkdown"></div>
+          </v-flex>
+          <v-flex xs6>
+            <ImageUpload ref="imgUpload" />
+          </v-flex>
+        </v-layout>
 
-  <v-flex xs6>
-    <ImageUpload ref="imgUpload" />
-  </v-flex>
-  <v-flex xs12 text-xs-center round my-5>
-    <v-btn color="info" dark v-on:click="postPortfolios">
-      <v-icon size="25" class="mr-2">fa-plus</v-icon>
-      작성하기
-    </v-btn>
-  </v-flex>
-</v-layout>
+        <v-flex xs12 text-xs-center round my-5>
+          <v-btn color="info" dark v-on:click="postPortfolios">
+            <v-icon size="25" class="mr-2">fa-plus</v-icon>작성하기
+          </v-btn>
+        </v-flex>
+      </div>
+      <!-- </v-layout> -->
+    </div>
+  </div>
 </template>
 
 <script>
-import ImageUpload from '@/components/ImageUpload'
-import FirebaseService from '@/services/FirebaseService'
-import ImgBanner from "../components/ImgBanner"
+import ImageUpload from "@/components/ImageUpload";
+import FirebaseService from "@/services/FirebaseService";
+import ImgBanner from "../components/ImgBanner";
 export default {
-  name: 'PortfolioWrite',
+  name: "PortfolioWrite",
   props: {
     title: {
       type: String,
@@ -47,7 +64,7 @@ export default {
     img: {
       type: String,
       default: ""
-    },
+    }
   },
   components: {
     ImageUpload,
@@ -55,18 +72,49 @@ export default {
   },
   data() {
     return {
-      file: '',
-      description: "The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-    }
+      file: "",
+      description:
+        "The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+    };
   },
   methods: {
     async postPortfolios() {
       this.img = await this.$refs.imgUpload.imageUpload();
-      FirebaseService.postPortfolio(this.title, this.body, this.img)
+      FirebaseService.postPortfolio(this.title, this.body, this.img);
       this.$router.push({
-        name: 'portfolio'
-      })
+        name: "portfolio"
+      });
     }
   }
 };
 </script>
+
+<style>
+#portfolioWrite {
+  margin: 30px 200px;
+  background-color: #f0f0f0;
+}
+#writeCard {
+  padding: 40px 30px;
+}
+#portfolioHeader {
+  margin-top: 100px;
+}
+
+@media screen and (max-width: 760px) {
+  #portfolioWrite {
+    margin: 30px 100px;
+  }
+  #writeCard {
+    padding: 20px;
+  }
+}
+@media screen and (max-width: 600px) {
+  #portfolioWrite {
+    margin: 20px 50px;
+  }
+  #writeCard {
+    padding: 40px 20px;
+  }
+}
+</style>
