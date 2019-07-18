cb1a4b64ab44a7837cec622998169fa0a4276b6f
Joowon Lee <marulover7@gmail.com>
2019-07-18 11:13:57 +0900

    portfoliopage layout 수정

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card>
+  <v-card id="portfolioCard">
     <router-link :to="{name:'portfolioDetail', params: {pid} }">
       <v-img :src="imgSrc" height="200px"></v-img>
     </router-link>
@@ -31,13 +31,21 @@ export default {
 </script>
 
 <style scoped>
+#portfolioCard{
+  border-radius: 4px;
+  border: 5px solid #2D0E0E;
+}
 #title {
+  font-family: 'Montserrat', sans-serif !important;
+  font-weight: bold;
   width: 150px;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
 }
 #content {
+  font-family: 'Montserrat', sans-serif !important;
+  font-size: 16px;
   display: -webkit-box;
   -webkit-line-clamp: 1;
   -webkit-box-orient: vertical;
